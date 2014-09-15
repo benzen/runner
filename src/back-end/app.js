6c2b617cd042d9c430efcf94d1a007cfd8944538
Benjamin Dreux <benjamin.dreux@code3.ca>
2014-09-15 08:17:06 -0400

    Add simple api to work with the front end

--- src/back-end/app.js
+++ src/back-end/app.js
@@ -1,18 +1,33 @@
 var port = 3000;
 
 var express = require('express'),
-    logger = require('morgan');
+    logger = require('morgan'),
+    bodyParser = require('body-parser');
 
 
 var app = express();
 
 app.locals.pretty = true;
 app.use(logger('dev'));
-
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use(express.static(__dirname + '/../front-end/'));
 
-app.get('/*', function(req, res){
+
+app.get('/^(api/)*', function(req, res){
   res.sendFile(req.path);
+});
+
+
+var tracks = [];
+
+app.post("/api/tracks",function(req,res){
+  tracks = req.body;
+  res.status(200).end();
+});
+
+app.get("/api/tracks", function(req,res){
+  res.json(tracks);
 });
 
 app.listen(port);
